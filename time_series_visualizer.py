090bc85b68e095b67cee736c62602b14d9c2148a
Richards-R <rr7366@gmail.com>
2023-11-02 14:20:01 +0000

    bar plot complete

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -7,14 +7,15 @@ register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=True)
-#print(df.describe())
+df["date"] = pd.to_datetime(df["date"])
+
 print('csv_data1', df)
 
 # Clean data
 df = df[(df['value'] <= df['value'].quantile(0.975)) & (df['value'] >= df['value'].quantile(0.025))]
 print('ggggggggg', df.shape)
 print('dtype1', df.dtypes)
-
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 def draw_line_plot():
     # Draw line plot
@@ -33,20 +34,36 @@ def draw_bar_plot():
     # Copy and modify data for monthly bar plot
     df_bar = df.copy()
        
-    df_bar['year'] = pd.to_datetime(df_bar['date']).dt.year
-    df_bar['month'] = pd.to_datetime(df_bar['date']).dt.month
-    print('csv_data55\n', df_bar)
+    df_bar['year'] = pd.to_datetime(df_bar['date'], errors='coerce').dt.year
+    df_bar['month'] = pd.to_datetime(df_bar['date'], errors='coerce').dt.month
+    print('csv_data55\n', df_bar.dtypes)
+    df_bar["month"] = df_bar["month"].apply(lambda data: months[data-1])
 
-    groupedData = df_bar.groupby(["year", "month"])
+    df_bar["month"] = pd.Categorical(df_bar["month"], categories=months)
+
+    df_pivot = pd.pivot_table(df_bar, values="value", index="year", columns="month", aggfunc=np.mean)
+
+    ax = df_pivot.plot(kind="bar")
+    fig = ax.get_figure()
+    fig.set_size_inches(7, 6)
+    ax.set_xlabel("Years")
+    ax.set_ylabel("Average Page Views")
+
+    groupedData = df_bar.groupby(['year', 'month'])
     monthly_averages = groupedData.aggregate({"value":np.mean})
     print('monthly_averages\n', monthly_averages)
     
    
     # Draw bar plot
-    #plt.bar(, height='200')
-
-    fig = plt.figure(frameon=False)
-
+    
+    #g = sns.barplot(monthly_averages)
+    #fig.figure(frameon=False)
+    #fig = plt.figure(frameon=False)
+    # g = monthly_averages.plot(kind='bar',figsize=(12,6))
+    # g.set_xlabel('Years')
+    # g.set_ylabel('value')
+    # g.legend(title= 'month')
+    # fig = g.figure
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
